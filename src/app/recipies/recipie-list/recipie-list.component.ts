54723eb5b2e09d5114545be318865f0378f8aba0
M-S-KHAN <sajawalkhan111@gmail.com>
2022-06-20 19:57:51 +0500

    - Static Setup done!

--- src/app/recipies/recipie-list/recipie-list.component.ts
+++ src/app/recipies/recipie-list/recipie-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipie } from '../recipie.model';
 
 @Component({
   selector: 'app-recipie-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipie-list.component.css']
 })
 export class RecipieListComponent implements OnInit {
+
+  recipies: Recipie[] = [
+    new Recipie('Test Recipie', 'This is simply a test', 'https://bigoven-res.cloudinary.com/image/upload/sweetandsourstickythaiboneless-3a944d.jpg'),
+    new Recipie('Test Recipie', 'This is simply a test', 'https://bigoven-res.cloudinary.com/image/upload/sweetandsourstickythaiboneless-3a944d.jpg'),
+  ];
 
   constructor() { }
 
